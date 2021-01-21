f8069c81afe5fc88c3cc2dbf8d6ae704026c57f4
Adrian Segura <asegura1306@yahoo.com>
2021-01-21 13:30:37 -0800

    Question number and Score displayed on front-end.

--- src/components/TriviaBoard/TriviaBoard.js
+++ src/components/TriviaBoard/TriviaBoard.js
@@ -10,15 +10,15 @@ class TriviaBoard extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            // currentIndex: 0,
             question: Questions[currentIndex].question,
             option1: Questions[currentIndex].option1,
             option2: Questions[currentIndex].option2,
             option3: Questions[currentIndex].option3,
             option4: Questions[currentIndex].option4,
             realAnswer: Questions[currentIndex].answer,
-
             score: 0,
+            questionNum: 0,
+
             userChoice: null,
             showAnswer: false,
 
@@ -26,6 +26,7 @@ class TriviaBoard extends React.Component {
         };
     }
     nextQuestion = () => {
+        // Current index is the position in the array of Questions (Question Number)
         currentIndex++;
         this.setState ({
             question: Questions[currentIndex].question,
@@ -34,16 +35,21 @@ class TriviaBoard extends React.Component {
             option3: Questions[currentIndex].option3,
             option4: Questions[currentIndex].option4,
             realAnswer: Questions[currentIndex].answer,
+            score: this.state.score
         })
  
     }
 
     checkAnswer = (userSelected) => {
         console.log(userSelected)
-        // this.setState({
-        //     userChoice:  
-        // })
-        
+        console.log(this.state.realAnswer)
+        if (userSelected === this.state.realAnswer) {
+            this.setState({
+                score: this.state.score++,
+            })
+        }
+        console.log(this.state.score)
+        this.nextQuestion();
     }
 
     // on button click increment through questions  array, updating the states that we used before
@@ -51,6 +57,19 @@ class TriviaBoard extends React.Component {
     render() {
         return (
             <div>
+                <Container>
+                    <Row className="d-flex justify-content-between">
+                        <col-4>
+                            <h1>Question {currentIndex+1} / 20</h1>
+                        </col-4>
+                        <col-4>
+                            Timer here
+                        </col-4>
+                        <col-4>
+                            <h1>Score: {this.state.score}</h1>
+                        </col-4>
+                    </Row>
+                </Container>
                 <Container className="d-flex justify-content-center">
                     <Row>
                         <col-12>
@@ -70,6 +89,7 @@ class TriviaBoard extends React.Component {
                         <ImportedButton onClick={this.checkAnswer} increment={1} choice={this.state.option4}> </ImportedButton>
                     </Row>
                 </Container>
+                
             </div>
         )
     }
